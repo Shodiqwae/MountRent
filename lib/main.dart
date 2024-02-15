45cd5ac331bb65c03b6caac27d49427de11a3df0
Dmiyo <muhammadnugroho262@gmail.com>
2024-02-15 16:35:39 +0700

    ambil yak

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mount_app/Page/detail2.dart';
+import 'Page/detail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: {
-          "/" :(context) => detai2(),
+          "/" :(context) => detail(),
         },
     );
   }
